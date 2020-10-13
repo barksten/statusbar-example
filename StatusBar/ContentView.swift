29f131db0b6ff1c1a20dcaec9fc58e91d70aef77
Anders Östlin <anders.ostlin@lm.se>
2020-10-13 16:04:46 +0200

    Initial Commit

---
+++ StatusBar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StatusBar
+//
+//  Created by Anders Östlin on 2020-10-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
